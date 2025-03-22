7779afd7da73f3db5c3ebbb147a4fdff98cf6623
Sushant0412 <mikesushant05@gmail.com>
2025-03-22 11:06:34 +0530

    Improved UI

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -82,7 +82,7 @@ export default function Chat() {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen max-w-4xl mx-auto p-4">
+    <div className="flex flex-col h-screen max-w-4xl mx-auto p-4 relative">
       <Avatar isAnimating={isAssistantSpeaking} />
       <div className="flex-1 overflow-y-auto mb-4 space-y-4">
         {messages.map((message) => (
@@ -105,7 +105,10 @@ export default function Chat() {
         ))}
       </div>
 
-      <form onSubmit={handleSubmit} className="flex gap-2">
+      <form
+        onSubmit={handleSubmit}
+        className="sticky bottom-10 flex gap-2 bg-white dark:bg-gray-900 p-4 rounded-xl shadow-lg"
+      >
         <VoiceInterface
           onSpeechInput={handleSpeechInput}
           onSpeechStart={handleSpeechStart}
